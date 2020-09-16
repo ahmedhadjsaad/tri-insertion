29fda656218aef8aa5cc8eb43fc8d9fa9afba0d2
unknown <ahmed.hadjsaad@iit.ens.tn>
2020-09-16 15:14:54 +0200

    Updates

---
+++ precache-manifest.5eba73a42c496ce8b59b56534eed00cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac45963895185fc69726a115995d0e2e",
+    "url": "/rcheckpoint-JSX.git/index.html"
+  },
+  {
+    "revision": "e636982ddfd94b0e5434",
+    "url": "/rcheckpoint-JSX.git/static/css/main.1db78f72.chunk.css"
+  },
+  {
+    "revision": "683e0c3b2e3719e94df0",
+    "url": "/rcheckpoint-JSX.git/static/js/2.997915af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rcheckpoint-JSX.git/static/js/2.997915af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e636982ddfd94b0e5434",
+    "url": "/rcheckpoint-JSX.git/static/js/main.1b922cc4.chunk.js"
+  },
+  {
+    "revision": "909827512a9184c823aa",
+    "url": "/rcheckpoint-JSX.git/static/js/runtime-main.1d635342.js"
+  },
+  {
+    "revision": "cbce1e0b5ed32663eff1200cf623245b",
+    "url": "/rcheckpoint-JSX.git/static/media/image.cbce1e0b.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rcheckpoint-JSX.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7bbc26863f89a5e20327ff1995ade1db",
+    "url": "/rcheckpoint-JSX.git/static/media/v.7bbc2686.mp4"
+  }
+]);
